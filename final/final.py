daf54474a120d021fecd89eaf5b1e796aa4bb64f
Brian Park <bcpark@ncsu.edu>
2022-11-15 22:57:32 -0500

    refactor

--- final/final.py
+++ final/final.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
-import argparse
 from math import log10
 import torch.optim as optim
-from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader
 from os.path import exists, basename
 from os import makedirs, remove
@@ -12,11 +10,22 @@ from torchvision.transforms import Compose, CenterCrop, ToTensor, Resize
 import torch.utils.data as data
 from os import listdir
 from os.path import join
-from PIL import Image
-import torch
 import torch.nn as nn
+import torch
+from PIL import Image
+from torchvision.transforms import ToTensor
+import os
+import numpy as np
 
 from model import SuperResolutionTwitter
+
+device = torch.device(
+    "mps"
+    if torch.backends.mps.is_available()
+    else "cuda"
+    if torch.cuda.is_available()
+    else "cpu"
+)
 
 
 def is_image_file(filename):
@@ -124,9 +133,9 @@ def get_test_set(upscale_factor):
     )
 
 
-def train(epoch):
+def train(data_loader, model, criterion, optimizer, epoch):
     epoch_loss = 0
-    for iteration, batch in enumerate(training_data_loader, 1):
+    for iteration, batch in enumerate(data_loader, 1):
         input, target = batch[0].to(device), batch[1].to(device)
 
         optimizer.zero_grad()
@@ -135,30 +144,24 @@ def train(epoch):
         loss.backward()
         optimizer.step()
 
-        print(
-            "===> Epoch[{}]({}/{}): Loss: {:.4f}".format(
-                epoch, iteration, len(training_data_loader), loss.item()
-            )
-        )
-
     print(
         "===> Epoch {} Complete: Avg. Loss: {:.4f}".format(
-            epoch, epoch_loss / len(training_data_loader)
+            epoch, epoch_loss / len(data_loader)
         )
     )
 
 
-def test():
+def test(data_loader, model, criterion):
     avg_psnr = 0
     with torch.no_grad():
-        for batch in testing_data_loader:
+        for batch in data_loader:
             input, target = batch[0].to(device), batch[1].to(device)
 
             prediction = model(input)
             mse = criterion(prediction, target)
             psnr = 10 * log10(1 / mse.item())
             avg_psnr += psnr
-    print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(testing_data_loader)))
+    print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(data_loader)))
 
 
 def checkpoint(epoch):
@@ -168,65 +171,74 @@ def checkpoint(epoch):
     pass
 
 
-if __name__ == "__main__":
-    # Training settings
-    parser = argparse.ArgumentParser(description="PyTorch Super Res Example")
-    parser.add_argument(
-        "--upscale_factor", type=int, default=3, help="super resolution upscale factor"
-    )
-    parser.add_argument("--batchSize", type=int, default=64, help="training batch size")
-    parser.add_argument(
-        "--testBatchSize", type=int, default=32, help="testing batch size"
-    )
-    parser.add_argument(
-        "--nEpochs", type=int, default=100, help="number of epochs to train for"
-    )
-    parser.add_argument(
-        "--lr", type=float, default=0.01, help="Learning Rate. Default=0.01"
-    )
-    parser.add_argument(
-        "--threads",
-        type=int,
-        default=8,
-        help="number of threads for data loader to use",
-    )
-    parser.add_argument(
-        "--seed", type=int, default=1337, help="random seed to use. Default=1337"
-    )
-    opt = parser.parse_args()
+def inference():
+    input_image = "data/BSDS300/images/test/16077.jpg"
+    model = "models/model_epoch_48.pth"
+    output_filename = "out.png"
 
-    device = torch.device(
-        "mps"
-        if torch.backends.mps.is_available()
-        else "cuda"
-        if torch.cuda.is_available()
-        else "cpu"
-    )
+    img = Image.open(input_image).convert("YCbCr")
+    y, cb, cr = img.split()
 
-    train_set = get_training_set(opt.upscale_factor)
-    test_set = get_test_set(opt.upscale_factor)
+    model = torch.load(model)
+    img_to_tensor = ToTensor()
+    input = img_to_tensor(y).view(1, -1, y.size[1], y.size[0])
+
+    model = model.to(device)
+    input = input.to(device)
+
+    out = model(input)
+    out = out.cpu()
+    out_img_y = out[0].detach().numpy()
+    out_img_y *= 255.0
+    out_img_y = out_img_y.clip(0, 255)
+    out_img_y = Image.fromarray(np.uint8(out_img_y[0]), mode="L")
+
+    out_img_cb = cb.resize(out_img_y.size, Image.Resampling.BICUBIC)
+    out_img_cr = cr.resize(out_img_y.size, Image.Resampling.BICUBIC)
+    out_img = Image.merge("YCbCr", [out_img_y, out_img_cb, out_img_cr]).convert("RGB")
+
+    out_img.save(output_filename)
+    print("output image saved to ", output_filename)
+
+
+def training():
+    upscale_factor = 3
+    threads = os.cpu_count()
+    batch_size = 64
+    test_batch_size = 32
+    epochs = 100
+    lr = 0.01
+
+    train_set = get_training_set(upscale_factor)
+    test_set = get_test_set(upscale_factor)
+
     training_data_loader = DataLoader(
         dataset=train_set,
-        num_workers=opt.threads,
-        batch_size=opt.batchSize,
+        num_workers=threads,
+        batch_size=batch_size,
         shuffle=True,
     )
     testing_data_loader = DataLoader(
         dataset=test_set,
-        num_workers=opt.threads,
-        batch_size=opt.testBatchSize,
+        num_workers=threads,
+        batch_size=test_batch_size,
         shuffle=False,
     )
 
-    model = SuperResolutionTwitter(upscale_factor=opt.upscale_factor).to(device)
+    model = SuperResolutionTwitter(upscale_factor=upscale_factor).to(device)
+
     criterion = nn.MSELoss()
-
-    optimizer = optim.Adam(model.parameters(), lr=opt.lr)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
-    0.0001
 
-    for epoch in range(1, opt.nEpochs + 1):
-        train(epoch)
-        test()
+    for epoch in range(1, epochs + 1):
+        train(training_data_loader, model, criterion, optimizer, epoch)
+        test(training_data_loader, model, criterion)
+        test(testing_data_loader, model, criterion)
         checkpoint(epoch)
         scheduler.step()
+
+
+if __name__ == "__main__":
+    # training()
+    inference()
